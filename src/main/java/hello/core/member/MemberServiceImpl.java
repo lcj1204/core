bc06d050967c5a75c51e8e7074520395bea7a7cc
lcj1204 <dlckdwo1204@naver.com>
2022-08-04 16:39:16 +0900

    MemberServiceImpl 생성

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,14 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    @Override
+    public void join(Member member) {
+
+    }
+
+    @Override
+    public Member findByName(Long memberId) {
+        return null;
+    }
+}
