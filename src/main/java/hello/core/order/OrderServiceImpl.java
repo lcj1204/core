f47bbbcc33d904ac73df5c0f111bb780e0b2e186
lcj1204 <dlckdwo1204@naver.com>
2022-08-07 18:22:24 +0900

    REFACTOR: 할인 정책의 구현체에 의존하지 않음으로 써 DIP 충족

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,8 @@ public class OrderServiceImpl implements OrderService{
      * 왜 private fianl 로 설정해야되나?
      */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy; // final은 지워줘야댐. final 은 꼭 값이 할당되어야 하니까
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
