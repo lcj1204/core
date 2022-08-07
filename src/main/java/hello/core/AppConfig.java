598c38ff4371c01f5e4c3fc9019597a0f2c2d85f
lcj1204 <dlckdwo1204@naver.com>
2022-08-07 19:31:16 +0900

    FEAT: MemberService 와 OrderService 구현체 지정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,4 +1,22 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
 public class AppConfig {
+    /**
+     * 이렇게 선언하는게 뭐하는건지 모르겠음
+     * 클래스의 객체를 만드는건가? 생성자인가?
+     */
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
 }
