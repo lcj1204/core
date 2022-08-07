0dc0c471cb308edd9a3d31186b3dcf60338a2f14
lcj1204 <dlckdwo1204@naver.com>
2022-08-07 16:30:51 +0900

    REFACTOR: 수정

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -3,6 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -17,22 +18,24 @@ class RateDiscountPolicyTest {
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
+    @DisplayName("VIP는 10% 할인 정책이 적용되어야 한다.")
     void vip_o() {
         //given
         Member memberVIP = new Member(1L, "memberVIP", Grade.VIP);
         //when
-        int discountVIP = discountPolicy.discount(memberVIP, 10000);
+        int discount = discountPolicy.discount(memberVIP, 10000);
         //then
-        assertThat(discountVIP).isEqualTo(1000);
+        assertThat(discount).isEqualTo(1000);
     }
 
     @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x() {
         //given
-        Member memberBASIC = new Member(1L, "memberBASIC", Grade.BASIC);
+        Member memberBASIC = new Member(2L, "memberBASIC", Grade.BASIC);
         //when
-        int discountBASIC = discountPolicy.discount(memberBASIC, 10000);
+        int discount = discountPolicy.discount(memberBASIC, 10000);
         //then
-        assertThat(discountBASIC).isEqualTo(0);
+        assertThat(discount).isEqualTo(0);
     }
 }
