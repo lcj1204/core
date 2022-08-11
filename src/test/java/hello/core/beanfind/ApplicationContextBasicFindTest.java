2c89913c52e0c2bff2912223c36343e8d885d135
lcj1204 <dlckdwo1204@naver.com>
2022-08-11 17:00:57 +0900

    REFACTOR: 실패 경우 Test 추가

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,9 +6,11 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -21,5 +23,29 @@ public class ApplicationContextBasicFindTest {
 //        System.out.println("memberService = " + memberService);
 //        System.out.println("memberServicegetClass() = " + memberService.getClass());
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 스프링 컨테이너에 객체가 등록이 되어있으면 구현체로도 조회 가능.
+    // 근데 역할이 아니라 구현에 의존한 코드는 좋은 객체지향이 아님.(그치만 살다보면 쓸 일이 있을 수도 있다.)
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 실패 Test
+    @Test
+    @DisplayName("빈에 등록되지 않은 이름을 조회할 때")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
     }
 }
